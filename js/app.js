4277434655cb7364d13f32a28aa08e64db71efb4
Matt Burger <matt.burger1@outlook.com>
2019-04-08 13:19:01 -0700

    basic layou

---
+++ js/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var picPool = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+
