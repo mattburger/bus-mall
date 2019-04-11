022861c9588c95b098b6f37dab583d4795937a32
Matt Burger <matt.burger1@outlook.com>
2019-04-11 12:58:42 -0700

    Dana set me str8

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ var allProducts = [];
 var lastViewedArr = [];
 var randArr = [];
 var busMallChart;
+var sessionClicks = 0;
 
 /*--------------constructor stuff--------------*/
 function MallProduct(name)
@@ -38,55 +39,8 @@ function rand(number)
   return (Math.floor(Math.random() * Math.floor(number)));
 }
 
-function genRandArr()
-{
-  var arr = [];
-  for(var i = 0; i < 3; i++)
-  {
-    arr.push(rand(20));
-  }
-  //console.log(arr);
-  return arr;
-}
-
 
 /*thanks to @Raynos with Stackoverflow */
-function xorSwapHalf(array)
-{
-  var i = null;
-  var r = null;
-  var length = array.length;
-  for (i = 0; i < length / 2; i += 1)
-  {
-    r = length - 1 - i;
-    var left = array[i];
-    var right = array[r];
-    left ^= right;
-    right ^= left;
-    left ^= right;
-    array[i] = left;
-    array[r] = right;
-  }
-  return array;
-}
-
-
-
-function checkDupLastView(rArr,lastArr)
-{
-  var someKey = null;
-  var rArr2 = rArr;
-  xorSwapHalf(rArr2);
-  for(var i = 0; i < rArr.length;i++)
-  {
-    if((rArr[i] === rArr2[i]) || rArr.includes(lastArr[i]))
-    {
-      someKey = i;
-      return 1;
-    }
-  }
-  return 0,someKey;
-}
 
 var prod1 = document.getElementById('prod1');
 var prod2 = document.getElementById('prod2');
@@ -99,24 +53,15 @@ var prodArr = [prod1,prod2,prod3];
 
 function generateProd()
 {
-  var counter = 0;
-  randArr = genRandArr();
-  var codes = checkDupLastView(randArr,lastViewedArr);
-  while(codes[0] === 1)
+  while(randArr.length < 3)
   {
-    randArr[codes[1]] = rand(20);
-    codes = checkDupLastView(randArr,lastViewedArr);
-    counter += 1;
-    console.log(codes[1]);
-    console.log('in while',randArr);
-    if(counter > 120)
+    var randArrVariable = rand(20);
+    if(!randArr.includes(randArrVariable) && !lastViewedArr.includes(randArrVariable))
     {
-      console.log('ewww this is stupid');
-      console.log('in while counter',counter);
-      return 'ARRRRGHHHHHH';
+      randArr.push(randArrVariable);
     }
+
   }
-  counter = 0;
 
   for(i = 0; i < prodArr.length;i++)
   {
@@ -148,26 +93,7 @@ function prepChartData()
     storedClicks.push(0);
     storedViews.push(0);
   }
-  //storedClicks = createClicksArr();
 }
-function checkClicks()
-{
-  var tmpTotal = 0;
-  for(var i = 0; i < allProducts.length; i++)
-  {
-    tmpTotal+=allProducts[i].clicks;
-  }
-  return tmpTotal;
-}
-/*function createClicksArr()
-{
-  var arr = [];
-  for(var i = 0; i < allProducts.length;i++)
-  {
-    arr.push(allProducts[i].clicks);
-  }
-  return arr;
-}*/
 function handleClick(id)
 {
   console.log('In evenHandler');
@@ -196,12 +122,16 @@ function handleClick(id)
   generateProd();
   /*ADD AN ELSEIF TO ACCOUNT FOR PREV LOCAL STORAGE */
 
-  if(checkClicks()>=25)
+  if(sessionClicks>=24)
   {
     //prepChartData();
     makeChart();
     document.getElementById('pickpic').removeEventListener('click',_listener);
 
+  }
+  else
+  {
+    sessionClicks++;
   }
 }
 
@@ -276,8 +206,3 @@ var _listener = function(event){
   }
 };
 document.getElementById('pickpic').addEventListener('click',_listener);
-
-/*
-console.log(sessionStorage.getItem(allProducts[0].storKey));
-console.log(allProducts[14].storKey);
-*/
